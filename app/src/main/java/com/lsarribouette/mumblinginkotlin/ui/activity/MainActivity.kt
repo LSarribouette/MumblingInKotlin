ae33616e71aa0681423009b0a51bc1476060fc06
Lauranne Sarribouette <lauranne.sarribouette@gmail.com>
2023-03-15 08:21:55 +0100

    Data Binding

--- app/src/main/java/com/lsarribouette/mumblinginkotlin/ui/activity/MainActivity.kt
+++ app/src/main/java/com/lsarribouette/mumblinginkotlin/ui/activity/MainActivity.kt
@@ -3,13 +3,19 @@ package com.lsarribouette.mumblinginkotlin.ui.activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.databinding.DataBindingUtil
 import com.lsarribouette.mumblinginkotlin.R
+import com.lsarribouette.mumblinginkotlin.bo.Produit
+import com.lsarribouette.mumblinginkotlin.databinding.ActivityMainBinding
 import com.lsarribouette.mumblinginkotlin.repository.ProduitRepository
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val amb: ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+//        amb.produit = Produit(4, "Encore un autre", 963.3f)
+        amb.produit = ProduitRepository.getProduit(2)
         Toast.makeText(
             this,
             ProduitRepository.getProduit(1).toString(),
